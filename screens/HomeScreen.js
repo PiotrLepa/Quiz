b4935630045e161791c4941e148d2e7d2e334cf6
Piotr Łępa <piotr.lepa@altconnect.pl>
2019-12-05 22:11:13 +0100

    fetch quizzes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,164 +10,28 @@ import {
 import AsyncStorage from '@react-native-community/async-storage';
 import SplashScreen from 'react-native-splash-screen';
 import {Navigation} from 'react-native-navigation';
-import {push, showDrawer, hideDrawerMenuIcon, navigateAndClearStack} from '../navigation/NavigationUtils';
+import {
+  push,
+  showDrawer,
+  hideDrawerMenuIcon,
+  navigateAndClearStack,
+} from '../navigation/NavigationUtils';
 import {
   REGULATIONS_SCREEN,
   QUIZ_SCREEN,
   SHOW_REGULATIONS_SCREEN_STORAGE,
+  BASE_URL
 } from '../Constants';
 
 import QuizContext from '../QuizContext';
 
-const getQuizzes = () => {
-  return [
-    [
-      {
-        question:
-          'Który wódz po śmierci Gajusza Mariusza, prowadził wojnę domową z Sullą?',
-        answers: [
-          {
-            content: 'Lucjusz Cynna',
-            isCorrect: true,
-          },
-          {
-            content: 'Lucjusz Ceezar',
-            isCorrect: false,
-          },
-          {
-            content: 'Lucjusz Murena',
-            isCorrect: false,
-          },
-          {
-            content: 'Marek Krassus',
-            isCorrect: false,
-          },
-        ],
-        duration: 30,
-      },
-      {
-        question: 'Który mamy wiek?',
-        answers: [
-          {
-            content: 'XIX',
-            isCorrect: false,
-          },
-          {
-            content: 'XX',
-            isCorrect: false,
-          },
-          {
-            content: 'XXI',
-            isCorrect: true,
-          },
-          {
-            content: 'XXV',
-            isCorrect: false,
-          },
-        ],
-        duration: 30,
-      },
-      {
-        question: '2+2*2 to: ',
-        answers: [
-          {
-            content: '8',
-            isCorrect: false,
-          },
-          {
-            content: '6',
-            isCorrect: true,
-          },
-          {
-            content: '12',
-            isCorrect: false,
-          },
-          {
-            content: '9',
-            isCorrect: false,
-          },
-        ],
-        duration: 30,
-      },
-    ],
-    [
-      {
-        question: '2+2*2 to: ',
-        answers: [
-          {
-            content: '8',
-            isCorrect: false,
-          },
-          {
-            content: '6',
-            isCorrect: true,
-          },
-          {
-            content: '12',
-            isCorrect: false,
-          },
-          {
-            content: '9',
-            isCorrect: false,
-          },
-        ],
-        duration: 15,
-      },
-      {
-        question:
-          'Który wódz po śmierci Gajusza Mariusza, prowadził wojnę domową z Sullą?',
-        answers: [
-          {
-            content: 'Lucjusz Cynna',
-            isCorrect: true,
-          },
-          {
-            content: 'Lucjusz Ceezar',
-            isCorrect: false,
-          },
-          {
-            content: 'Lucjusz Murena',
-            isCorrect: false,
-          },
-          {
-            content: 'Marek Krassus',
-            isCorrect: false,
-          },
-        ],
-        duration: 15,
-      },
-      {
-        question: 'Który mamy wiek?',
-        answers: [
-          {
-            content: 'XIX',
-            isCorrect: false,
-          },
-          {
-            content: 'XX',
-            isCorrect: false,
-          },
-          {
-            content: 'XXI',
-            isCorrect: true,
-          },
-          {
-            content: 'XXV',
-            isCorrect: false,
-          },
-        ],
-        duration: 15,
-      },
-    ],
-  ];
-};
-
 const HomeScreen = ({componentId}) => {
-  const [quizzesData, setQuizzesData] = useState(getQuizzes());
+  const [quizzesData, setQuizzesData] = useState();
 
   const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
+    fetchQuizzes();
     SplashScreen.hide();
     Navigation.events().registerNavigationButtonPressedListener(
       ({componentId}) => showDrawer(componentId),
@@ -178,27 +42,42 @@ const HomeScreen = ({componentId}) => {
         navigateAndClearStack(componentId, REGULATIONS_SCREEN);
       }
     });
-  });
+  }, [0]);
+
+  const fetchQuizzes = () => {
+    fetch(BASE_URL + 'tests')
+      .then(response => response.json())
+      .then(data => setQuizzesData(data))
+      .catch(reason => console.log(reason));
+  };
 
   const refreshQuizzes = () => {
     setIsRefreshing(true);
     setTimeout(() => setIsRefreshing(false), 1500);
   };
 
-  const createItem = (quiz, index) => {
+  const createItem = (
+    {id, name, description, tags, level, numberOfTasks},
+    index,
+  ) => {
     return (
       <View style={styles.quizContainer}>
         <TouchableOpacity
-          key={Math.random() * 10000 * Math.random()}
+          key={id}
           onPress={() => {
             QuizContext.setCurrentQuiz(quiz);
             push(componentId, QUIZ_SCREEN);
           }}>
-          <Text style={styles.quizTitle}>Quiz #{index + 1}</Text>
-          <Text style={styles.quizDescription}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            volutpat lectus et justo lacinia pharetra. Aliquam rutrum maximus
-            gravida. Aliquam et sapien neque.
+          <Text style={styles.quizTitle}>
+            Quiz #{index + 1} {name}
+          </Text>
+          <Text style={styles.quizDescription}>{description}</Text>
+          <Text style={styles.quizInfo}>
+            Tags: {tags.join(',')}
+          </Text>
+          <Text style={styles.quizInfo}>Level: {level}</Text>
+          <Text style={styles.quizInfo}>
+            Number Of Tasks: {numberOfTasks}
           </Text>
         </TouchableOpacity>
       </View>
@@ -222,7 +101,7 @@ const HomeScreen = ({componentId}) => {
         <FlatList
           data={quizzesData}
           renderItem={({item, index}) => createItem(item, index)}
-          keyExtractor={(item, index) => index.toString()}
+          keyExtractor={(item) => item.id}
           refreshControl={
             <RefreshControl
               onRefresh={refreshQuizzes}
@@ -261,10 +140,17 @@ const styles = StyleSheet.create({
   },
   quizDescription: {
     textAlign: 'center',
-    fontSize: 15,
+    fontSize: 20,
     color: 'white',
     padding: 8,
     fontFamily: 'Lato-Regular',
+  },
+  quizInfo: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: 'white',
+    padding: 8,
+    fontFamily: 'Lato-Light',
   },
 });
 
