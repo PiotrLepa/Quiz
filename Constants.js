cb7fbf25f85c2a6750d973c8aae03be876bbb723
Piotr Łępa <piotr.lepa@altconnect.pl>
2019-12-05 21:25:04 +0100

    fetch results from api

--- Constants.js
+++ Constants.js
@@ -8,4 +8,4 @@ export const RESULTS_SCREEN = 'ResultsScreen';
 export const SHOW_REGULATIONS_SCREEN_STORAGE =
   'SHOW_REGULATIONS_SCREEN_STORAGE';
 
-export const BASE_URL = 'https://tgryl.pl/quiz/';
+export const BASE_URL = 'https://pwsz-quiz-api.herokuapp.com/api/';
--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -3,120 +3,74 @@ import {StyleSheet, View, Text, FlatList, RefreshControl} from 'react-native';
 import QuizContext from '../QuizContext';
 import {BASE_URL} from '../Constants';
 
-const getResults = () => {
-  return [
-    {
-      nick: 'Tomek',
-      score: 5,
-      total: 7,
-      type: 'matematyka',
-      date: '2019-11-16',
-    },
-    {
-      nick: 'Adam',
-      score: 7,
-      total: 9,
-      type: 'chemia',
-      date: '2019-11-21',
-    },
-    {
-      nick: 'Edward',
-      score: 22,
-      total: 33,
-      type: 'biologia',
-      date: '2019-10-01',
-    },
-    {
-      nick: 'Marek',
-      score: 18,
-      total: 20,
-      type: 'historia',
-      date: '2019-11-01',
-    },
-  ];
-};
+const ResultsScreen = () => {
 
-class ResultsScreen extends React.Component {
-  componentDidMount() {
-    console.log(`url: ${BASE_URL + 'results'}`);
+  useEffect(() => {
+    fetchResults()
+  },[0]);
+
+  const [resultsData, setResultsData] = useState();
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const refreshResults = () => {
+    setIsRefreshing(true);
+    setTimeout(() => setIsRefreshing(false), 1500);
+  };
+
+  const fetchResults = () => {
     fetch(BASE_URL + 'results')
-      .then(response => console.log('response: ', response))
+      .then(response => response.json())
+      .then(data => setResultsData(data))
       .catch(reason => console.log(reason));
-  }
+  };
 
-  // useEffect(async () => {
-  //   // fetchResults();
-  //   const response = await fetch(BASE_URL + 'results');
-  //   console.log(response)
-  //   setResultsData(response.json);
-  // });
+  const createItem = item => {
+    return (
+      <View
+        style={styles.resultContainer}
+        key={Math.random() * 10000 * Math.random()}>
+        <Text style={styles.resultText}>Nick: {item.nick}</Text>
+        <Text style={styles.resultText}>Score: {item.score}</Text>
+        <Text style={styles.resultText}>Total: {item.total}</Text>
+        <Text style={styles.resultText}>Type: {item.type}</Text>
+        <Text style={styles.resultText}>Date: {item.date}</Text>
+      </View>
+    );
+  };
 
-  // const [resultsData, setResultsData] = useState();
+  const renderCurrentQuizResult = () => {
+    const result = QuizContext.getPointsResult();
+    QuizContext.clear();
 
-  // const [isRefreshing, setIsRefreshing] = useState(false);
+    if (result === null) return <View />;
+    return (
+      <Text style={styles.userResult}>
+        You answered {result.score} of {result.maxPoints} correctly
+      </Text>
+    );
+  };
 
-  // const refreshResults = () => {
-  //   setIsRefreshing(true);
-  //   setTimeout(() => setIsRefreshing(false), 1500);
-  // };
-
-  // const fetchResults = () => {
-  //   console.log(`url: ${BASE_URL + 'results'}`)
-  //   fetch(BASE_URL + 'results')
-  //     .then(response => console.log('response: ', response.json))
-  //     .catch(reason => console.log(reason.stack));
-  // };
-
-  // const createItem = item => {
-  //   return (
-  //     <View
-  //       style={styles.resultContainer}
-  //       key={Math.random() * 10000 * Math.random()}>
-  //       <Text style={styles.resultText}>Nick: {item.nick}</Text>
-  //       <Text style={styles.resultText}>Score: {item.score}</Text>
-  //       <Text style={styles.resultText}>Total: {item.total}</Text>
-  //       <Text style={styles.resultText}>Type: {item.type}</Text>
-  //       <Text style={styles.resultText}>Date: {item.date}</Text>
-  //     </View>
-  //   );
-  // };
-
-  // const renderCurrentQuizResult = () => {
-  //   const result = QuizContext.getPointsResult();
-  //   QuizContext.clear();
-
-  //   if (result === null) return <View />;
-  //   return (
-  //     <Text style={styles.userResult}>
-  //       You answered {result.score} of {result.maxPoints} correctly
-  //     </Text>
-  //   );
-  // };
-
-  // return (
-  //   <>
-  //     <View style={styles.container}>
-  //       {renderCurrentQuizResult()}
-  //       <FlatList
-  //         data={resultsData}
-  //         renderItem={({item}) => createItem(item)}
-  //         keyExtractor={(item, index) => index.toString()}
-  //         refreshControl={
-  //           <RefreshControl
-  //             onRefresh={refreshResults}
-  //             refreshing={isRefreshing}
-  //             tintColor="dodgerblue"
-  //             colors={['dodgerblue']}
-  //           />
-  //         }
-  //       />
-  //     </View>
-  //   </>
-  // );
-
-  render() {
-    return <Text>fsdfsd</Text>;
-  }
+  return (
+    <>
+      <View style={styles.container}>
+        {renderCurrentQuizResult()}
+        <FlatList
+          data={resultsData}
+          renderItem={({item}) => createItem(item)}
+          keyExtractor={(item, index) => index.toString()}
+          refreshControl={
+            <RefreshControl
+              onRefresh={refreshResults}
+              refreshing={isRefreshing}
+              tintColor="dodgerblue"
+              colors={['dodgerblue']}
+            />
+          }
+        />
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
